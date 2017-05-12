af9bfe0a4bd17d79d6654b4730f2848b49fe5224
Jack <jagandecapri@gmail.com>
2017-05-12 22:11:34 +0800

    Add window sliding module
    Window sliding module accumulates packets in delta_t.
    Then these accumulated packet is accumulated in an array of capacity 15.

--- orunada/main.go
+++ orunada/main.go
@@ -7,10 +7,138 @@ import (
 	"fmt"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
+	"strings"
+	"time"
 )
+
+func printPacketInfo(packet gopacket.Packet) {
+	// Let's see if the packet is an ethernet packet
+	ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
+	if ethernetLayer != nil {
+		fmt.Println("Ethernet layer detected.")
+		ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
+		fmt.Println("Source MAC: ", ethernetPacket.SrcMAC)
+		fmt.Println("Destination MAC: ", ethernetPacket.DstMAC)
+		// Ethernet type is typically IPv4 but could be ARP or other
+		fmt.Println("Ethernet type: ", ethernetPacket.EthernetType)
+		fmt.Println()
+	}
+
+	// Let's see if the packet is IP (even though the ether type told us)
+	ipLayer := packet.Layer(layers.LayerTypeIPv4)
+	if ipLayer != nil {
+		fmt.Println("IPv4 layer detected.")
+		ip, _ := ipLayer.(*layers.IPv4)
+
+		// IP layer variables:
+		// Version (Either 4 or 6)
+		// IHL (IP Header Length in 32-bit words)
+		// TOS, Length, Id, Flags, FragOffset, TTL, Protocol (TCP?),
+		// Checksum, SrcIP, DstIP
+		fmt.Printf("From %s to %s\n", ip.SrcIP, ip.DstIP)
+		fmt.Println("Protocol: ", ip.Protocol)
+		fmt.Println()
+	}
+
+	// Let's see if the packet is TCP
+	tcpLayer := packet.Layer(layers.LayerTypeTCP)
+	if tcpLayer != nil {
+		fmt.Println("TCP layer detected.")
+		tcp, _ := tcpLayer.(*layers.TCP)
+
+		// TCP layer variables:
+		// SrcPort, DstPort, Seq, Ack, DataOffset, Window, Checksum, Urgent
+		// Bool flags: FIN, SYN, RST, PSH, ACK, URG, ECE, CWR, NS
+		fmt.Printf("From port %d to %d\n", tcp.SrcPort, tcp.DstPort)
+		fmt.Println("Sequence number: ", tcp.Seq)
+		fmt.Println(tcp.FIN, tcp.SYN, tcp.RST, tcp.PSH, tcp.ACK, tcp.URG, tcp.ECE, tcp.CWR, tcp.NS)
+		fmt.Println()
+	}
+
+	// Iterate over all layers, printing out each layer type
+	fmt.Println("All packet layers:")
+	for _, layer := range packet.Layers() {
+		fmt.Println("- ", layer.LayerType())
+	}
+
+	// When iterating through packet.Layers() above,
+	// if it lists Payload layer then that is the same as
+	// this applicationLayer. applicationLayer contains the payload
+	applicationLayer := packet.ApplicationLayer()
+	if applicationLayer != nil {
+		fmt.Println("Application layer/Payload found.")
+		fmt.Printf("%s\n", applicationLayer.Payload())
+
+		// Search for a string inside the payload
+		if strings.Contains(string(applicationLayer.Payload()), "HTTP") {
+			fmt.Println("HTTP found!")
+		}
+	}
+
+	// Check for errors
+	if err := packet.ErrorLayer(); err != nil {
+		fmt.Println("Error decoding some part of the packet:", err)
+	}
+}
+
+var delta_t time.Duration = 300 * time.Millisecond
+var time_counter time.Time
+
+type PacketData struct{
+	data gopacket.Packet
+	metadata gopacket.CaptureInfo
+}
+
+type PacketAcc []PacketData
+
+func Preprocess(ch chan PacketData, acc chan PacketAcc, quit chan int){
+	var tmp PacketAcc
+	for{
+		select{
+		case pd := <- ch:
+			packet_time := pd.metadata.Timestamp
+			if time_counter.IsZero() {
+				//fmt.Println("Initialize Time")
+				time_counter = packet_time
+			} else if packet_time.Before(time_counter.Add(delta_t)) || packet_time.Equal(time_counter.Add(delta_t)) {
+				//fmt.Println("packet_time <= time_counter")
+				if tmp == nil{
+					tmp = []PacketData{pd}
+				} else if tmp != nil {
+					tmp = append(tmp, pd)
+				}
+			} else if packet_time.After(time_counter.Add(delta_t)) {
+				//fmt.Println("packet_time > time_counter")
+				acc <- tmp
+				tmp = nil
+				time_counter = time_counter.Add(delta_t)
+			}
+		case <-quit:
+			fmt.Println("No Data")
+			close(ch)
+			close(acc)
+		}
+	}
+}
+
+func WindowTimeSlide(acc chan PacketAcc){
+	var arr [15]PacketAcc
+	packet_acc := <- acc
+	if len(arr) != 15 {
+		arr[len(arr) - 1] = packet_acc
+	} else {
+		arr[0] = packet_acc
+	}
+	fmt.Println(arr[0][0].data)
+}
 
 func main() {
 	handleRead, err := pcap.OpenOffline("C:\\Users\\Jack\\Downloads\\201705021400.pcap")
+	ch := make(chan PacketData)
+	acc := make(chan PacketAcc)
+	quit := make(chan int)
+	go Preprocess(ch, acc, quit)
+	go WindowTimeSlide(acc)
 
 	if(err != nil){
 		log.Fatal(err)
@@ -19,15 +147,15 @@ func main() {
 	for {
 		data, ci, err := handleRead.ReadPacketData()
 		if err != nil && err != io.EOF {
+			quit <- 0
 			log.Fatal(err)
 		} else if err == io.EOF {
+			quit <- 0
 			break
 		} else {
 			packet := gopacket.NewPacket(data, layers.LayerTypeEthernet, gopacket.Default)
-			// Iterate over all layers, printing out each layer type
-			for _, layer := range packet.Layers() {
-				fmt.Println(packet.String(), layer.LayerType(), ci)
-			}
+			ch <- PacketData{packet, ci}
+			// printPacketInfo(packet)
 		}
 	}
 
