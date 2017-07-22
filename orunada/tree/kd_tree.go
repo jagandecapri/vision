962a0bbcaaf68fdca80109c54bf08905146abf27
Jack <jagandecapri@gmail.com>
2017-07-22 21:40:19 +0800

    Add two methods to interface
    Method Distance and PlaneDistance is added to
    for clustering algorithm later.

--- orunada/tree/kd_tree.go
+++ orunada/tree/kd_tree.go
@@ -6,9 +6,9 @@ type Point interface {
 	// Return the value X_{dim}, dim is started from 0
 	GetValue(dim int) int
 	// Return the distance between two points
-	//Distance(point Point) float64
+	Distance(point Point) float64
 	// Return the distance between the point and the plane X_{dim}=val
-	//PlaneDistance(val float64, dim int) float64
+	PlaneDistance(val float64, dim int) float64
 }
 
 type KDTree struct{
