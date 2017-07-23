07b6db7e899654de196566214ad89b2ee3fb923a
Jack <jagandecapri@gmail.com>
2017-07-23 16:46:41 +0800

    Use type assertion in Distance func
    Distance func needs to accept Point interface which
    will have the concrete type PointContainer in it.
    Use type assertion inside this function.

--- orunada/tree/point_container.go
+++ orunada/tree/point_container.go
@@ -15,15 +15,17 @@ func (p PointContainer) GetValue(dim int) int{
 	return p.point[dim]
 }
 
-func (p *PointContainer) Distance(p1 *PointContainer) float64{
+func (p PointContainer) Distance(p1 Point) float64{
 	sum := 0.0
+	t := p1.(*PointContainer)
 	for i:=0; i<len(p.point); i++{
-		sum += math.Pow(float64(p.point[i]-p1.point[i]), 2)
+		sum += math.Pow(float64(p.point[i]-t.point[i]), 2)
 	}
 	euclidean_dist := math.Sqrt(sum)
 	return euclidean_dist
+	return 0.0
 }
 
-func (p *PointContainer) PlaneDistance(val float64, dim int) float64{
+func (p PointContainer) PlaneDistance(val float64, dim int) float64{
 	return 0.0
 }
