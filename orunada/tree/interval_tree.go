08088ebf966f3bac56831a4ef630883c550095f3
Jack <jagandecapri@gmail.com>
2017-08-05 14:55:51 +0800

    Fix wrong import in interrval tree
    Imports should be only golang-collections

--- orunada/tree/interval_tree.go
+++ orunada/tree/interval_tree.go
@@ -1,28 +1,27 @@
 package tree
 
-import "github.com/Workiva/go-datastructures/augmentedtree"
+import "github.com/golang-collections/go-datastructures/augmentedtree"
 
-type Interval struct {
+type IntervalConc struct {
 	id int
 	low []int64
 	high []int64
 }
 
-// LowAtDimension returns an integer representing the lower bound
-// at the requested dimension.
-func (itv *Interval) LowAtDimension(dim uint64) int64{
+func (itv IntervalConc) LowAtDimension(dim uint64) int64{
 	return itv.low[dim - 1]
 }
 
 // HighAtDimension returns an integer representing the higher bound
 // at the requested dimension.
-func (itv *Interval) HighAtDimension(dim uint64) int64{
+func (itv IntervalConc) HighAtDimension(dim uint64) int64{
 	return itv.high[dim - 1]
 }
 
 // OverlapsAtDimension should return a bool indicating if the provided
 // interval overlaps this interval at the dimension requested.
-func (itv *Interval) OverlapsAtDimension(interval Interval, dim uint64) bool{
+func (itv IntervalConc) OverlapsAtDimension(interval augmentedtree.Interval, dim uint64) bool{
+	interval = interval.(IntervalConc)
 	if itv.LowAtDimension(dim) <= interval.LowAtDimension(dim) &&
 		itv.HighAtDimension(dim) > interval.HighAtDimension(dim){
 		return true
@@ -33,8 +32,22 @@ func (itv *Interval) OverlapsAtDimension(interval Interval, dim uint64) bool{
 // ID should be a unique ID representing this interval.  This
 // is used to identify which interval to delete from the tree if
 // there are duplicates.
-func (itv *Interval) ID() uint64{
+func (itv IntervalConc) ID() uint64{
 	return uint64(itv.id)
+}
+
+func IntervalBuilder(min int, max int, interval_length int) []IntervalConc {
+	id := 0
+	intervals := []IntervalConc{}
+	for i := min; i < max; i += interval_length{
+		for j := min; j < max; j += interval_length{
+			intervals = append(intervals, IntervalConc{	id: id,
+				low: []int64{int64(i), int64(i + interval_length)},
+				high: []int64{int64(j), int64(j + interval_length)}})
+			id += 1
+		}
+	}
+	return intervals
 }
 
 func NewIntervalTree(dim uint64) augmentedtree.Tree{
--- orunada/tree/interval_tree_test.go
+++ orunada/tree/interval_tree_test.go
@@ -3,23 +3,28 @@ package tree
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
+	"github.com/golang-collections/go-datastructures/augmentedtree"
 )
 
 func TestInterval_OverlapsAtDimension(t *testing.T) {
 	tests := []struct {
-		interval Interval
-		interval_test Interval
-		expected bool
-	}{{interval: Interval{low: []int64{0,0}, high: []int64{999,999}},
-		interval_test: Interval{low: []int64{0,2}, high: []int64{998,997}},
+		interval      IntervalConc
+		interval_test IntervalConc
+		expected      bool
+	}{{interval: IntervalConc{low: []int64{0,0}, high: []int64{999,999}},
+		interval_test: IntervalConc{low: []int64{0,2}, high: []int64{998,997}},
 		expected: true,
 	},
-	{interval: Interval{low: []int64{0,0}, high: []int64{999,999}},
-		interval_test: Interval{low: []int64{0,2}, high: []int64{999,999}},
+	{interval: IntervalConc{low: []int64{0,0}, high: []int64{999,999}},
+		interval_test: IntervalConc{low: []int64{0,2}, high: []int64{999,999}},
 		expected: false,
 	}}
 
 	for _, v := range tests{
 		assert.Equal(t, v.expected, v.interval.OverlapsAtDimension(v.interval_test, 1), "%v\n", v)
 	}
+}
+
+func TestInterval_ImplementsInterface(t *testing.T) {
+	assert.Implements(t, (*augmentedtree.Interval)(nil), new(IntervalConc))
 }
