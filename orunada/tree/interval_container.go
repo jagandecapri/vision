285f77422745c837323381fdece2aacff3207cc5
Jack <jagandecapri@gmail.com>
2017-08-26 15:22:20 +0800

    Rename interval_tree to interval_container
    interval_container contains the struct that implements
    the Interval interface in augmentedtree package.

--- orunada/tree/interval_container.go
+++ orunada/tree/interval_container.go
@@ -5,27 +5,27 @@ import (
 	"math"
 )
 
-type IntervalConc struct {
+type IntervalContainer struct {
 	Id   int
-	Interval_conc_store []IntervalConc
+	Interval_conc_store []IntervalContainer
 	Low  []float64
 	High []float64
 	Decimal_places int
 }
 
-func (itv IntervalConc) LowAtDimension(dim uint64) int64{
+func (itv IntervalContainer) LowAtDimension(dim uint64) int64{
 	return int64(itv.Low[dim - 1] * math.Pow(10, float64(itv.Decimal_places)))
 }
 
 // HighAtDimension returns an integer representing the higher bound
 // at the requested dimension.
-func (itv IntervalConc) HighAtDimension(dim uint64) int64{
+func (itv IntervalContainer) HighAtDimension(dim uint64) int64{
 	return int64(itv.High[dim - 1] * math.Pow(10, float64(itv.Decimal_places)))
 }
 
 // OverlapsAtDimension should return a bool indicating if the provided
 // interval overlaps this interval at the dimension requested.
-func (itv IntervalConc) OverlapsAtDimension(interval augmentedtree.Interval, dim uint64) bool{
+func (itv IntervalContainer) OverlapsAtDimension(interval augmentedtree.Interval, dim uint64) bool{
 	check := false
 	for i := uint64(1); i <= uint64(len(itv.Low)); i++{
 		if interval.LowAtDimension(i) <= itv.HighAtDimension(i) &&
@@ -41,16 +41,16 @@ func (itv IntervalConc) OverlapsAtDimension(interval augmentedtree.Interval, dim
 // ID should be a unique ID representing this interval.  This
 // is used to identify which interval to delete from the tree if
 // there are duplicates.
-func (itv IntervalConc) ID() uint64{
+func (itv IntervalContainer) ID() uint64{
 	return uint64(itv.Id)
 }
 
-func IntervalBuilder(min int, max int, interval_length int) []IntervalConc {
+func IntervalBuilder(min int, max int, interval_length int) []IntervalContainer {
 	id := 1
-	intervals := []IntervalConc{}
+	intervals := []IntervalContainer{}
 	for i := min; i < max; i += interval_length{
 		for j := min; j < max; j += interval_length{
-			intervals = append(intervals, IntervalConc{	Id: id,
+			intervals = append(intervals, IntervalContainer{	Id: id,
 				Low: []float64{float64(i), float64(j)},
 				High: []float64{float64(i + interval_length), float64(j + interval_length)}})
 			id += 1
--- orunada/tree/interval_container_test.go
+++ orunada/tree/interval_container_test.go
@@ -8,15 +8,15 @@ import (
 
 func TestInterval_OverlapsAtDimension(t *testing.T) {
 	tests := []struct {
-		interval      IntervalConc
-		interval_test IntervalConc
+		interval      IntervalContainer
+		interval_test IntervalContainer
 		expected      bool
-	}{{interval: IntervalConc{Low: []float64{0.0,0.0}, High: []float64{0.999,0.999}, Decimal_places: 5},
-		interval_test: IntervalConc{Low: []float64{0,0.2}, High: []float64{0.998,0.997}, Decimal_places: 5},
+	}{{interval: IntervalContainer{Low: []float64{0.0,0.0}, High: []float64{0.999,0.999}, Decimal_places: 5},
+		interval_test: IntervalContainer{Low: []float64{0,0.2}, High: []float64{0.998,0.997}, Decimal_places: 5},
 		expected: true,
 	},
-	{interval: IntervalConc{Low: []float64{0.0,0.0}, High: []float64{0.999,0.999}, Decimal_places: 5},
-		interval_test: IntervalConc{Low: []float64{1.0,5.0}, High: []float64{1.2,5.5}, Decimal_places: 5},
+	{interval: IntervalContainer{Low: []float64{0.0,0.0}, High: []float64{0.999,0.999}, Decimal_places: 5},
+		interval_test: IntervalContainer{Low: []float64{1.0,5.0}, High: []float64{1.2,5.5}, Decimal_places: 5},
 		expected: false,
 	}}
 
@@ -26,7 +26,7 @@ func TestInterval_OverlapsAtDimension(t *testing.T) {
 }
 
 func TestInterval_ImplementsInterface(t *testing.T) {
-	assert.Implements(t, (*augmentedtree.Interval)(nil), new(IntervalConc))
+	assert.Implements(t, (*augmentedtree.Interval)(nil), new(IntervalContainer))
 }
 
 func TestCreateIntervalTree(t *testing.T){
