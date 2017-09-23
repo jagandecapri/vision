92bf2e1c3dd7174e7f47daad98cdc157603482e3
Jack <jagandecapri@gmail.com>
2017-09-23 22:29:32 +0800

    Fix builder.go logic

--- orunada/tree/builder.go
+++ orunada/tree/builder.go
@@ -1,16 +1,34 @@
 package tree
 
-import "github.com/golang-collections/go-datastructures/augmentedtree"
+import (
+	"github.com/golang-collections/go-datastructures/augmentedtree"
+	"strconv"
+	"math/big"
+)
 
-func IntervalBuilder(min float64, max float64, interval_length float64) []IntervalContainer {
+func IntervalBuilder(min float64, max float64, interval_length float64, scale_factor int) []IntervalContainer {
 	id := 1
 	intervals := []IntervalContainer{}
-	for i := min; i < max; i += interval_length{
-		for j := min; j < max; j += interval_length{
+	i := min
+	for i < max{
+		j := min
+		for j < max{
 			intervals = append(intervals, IntervalContainer{	Id: id,
-				Low: []float64{float64(i), float64(j)},
-				High: []float64{float64(i + interval_length), float64(j + interval_length)}})
+				Low: []float64{i, j},
+				High: []float64{i + interval_length,
+					j + interval_length},
+				Scale_factor: scale_factor})
 			id += 1
+			j += interval_length
+			j, _ = strconv.ParseFloat(new(big.Float).SetFloat64(j).Text('f', 1), 64)
+			if j >= max{
+				break
+			}
+		}
+		i += interval_length
+		i, _ = strconv.ParseFloat(new(big.Float).SetFloat64(i).Text('f', 1), 64)
+		if i >= max{
+			break
 		}
 	}
 	return intervals
