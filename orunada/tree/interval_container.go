7eb305a9f6d17257bfbfd4ef9f4b6e6327bb6097
Jack <jagandecapri@gmail.com>
2017-09-23 22:27:02 +0800

    Add GetCenter() method to interval_container.go

--- orunada/tree/interval_container.go
+++ orunada/tree/interval_container.go
@@ -6,21 +6,22 @@ import (
 )
 
 type IntervalContainer struct {
-	Id   int
-	Interval_conc_store []IntervalContainer
-	Low  []float64
-	High []float64
-	Decimal_places int
+	Id           int
+	Low          []float64
+	High         []float64
+	Scale_factor int
 }
 
 func (itv IntervalContainer) LowAtDimension(dim uint64) int64{
-	return int64(itv.Low[dim - 1] * math.Pow(10, float64(itv.Decimal_places)))
+	tmp :=  int64(itv.Low[dim - 1] * math.Pow(10, float64(itv.Scale_factor)))
+	return tmp
 }
 
 // HighAtDimension returns an integer representing the higher bound
 // at the requested dimension.
 func (itv IntervalContainer) HighAtDimension(dim uint64) int64{
-	return int64(itv.High[dim - 1] * math.Pow(10, float64(itv.Decimal_places)))
+	tmp := int64(itv.High[dim - 1] * math.Pow(10, float64(itv.Scale_factor)))
+	return tmp
 }
 
 // OverlapsAtDimension should return a bool indicating if the provided
@@ -43,4 +44,13 @@ func (itv IntervalContainer) OverlapsAtDimension(interval augmentedtree.Interval
 // there are duplicates.
 func (itv IntervalContainer) ID() uint64{
 	return uint64(itv.Id)
+}
+
+func (itv IntervalContainer) GetCenter() []float64{
+	dim := len(itv.Low)
+	center := []float64{}
+	for i := 0; i < dim; i++{
+		center = append(center, (itv.High[i] - itv.Low[i])/2 )
+	}
+	return center
 }
--- orunada/tree/interval_container_test.go
+++ orunada/tree/interval_container_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"github.com/stretchr/testify/assert"
 	"github.com/golang-collections/go-datastructures/augmentedtree"
+	"fmt"
 )
 
 func TestInterval_OverlapsAtDimension(t *testing.T) {
@@ -11,16 +12,16 @@ func TestInterval_OverlapsAtDimension(t *testing.T) {
 		interval      IntervalContainer
 		interval_test IntervalContainer
 		expected      bool
-	}{{interval: IntervalContainer{Low: []float64{0.0,0.0}, High: []float64{0.999,0.999}, Decimal_places: 5},
-		interval_test: IntervalContainer{Low: []float64{0,0.2}, High: []float64{0.998,0.997}, Decimal_places: 5},
+	}{{interval: IntervalContainer{Low: []float64{0.0,0.0}, High: []float64{0.999,0.999}, Scale_factor: 5},
+		interval_test: IntervalContainer{Low: []float64{0,0.2}, High: []float64{0.998,0.997}, Scale_factor: 5},
 		expected: true,
 	},
-	{interval: IntervalContainer{Low: []float64{0.0000,0.0000}, High: []float64{0.0000,0.0000}, Decimal_places: 5},
-		interval_test: IntervalContainer{Low: []float64{0.0,0.0}, High: []float64{1.0,1.0}, Decimal_places: 5},
+	{interval: IntervalContainer{Low: []float64{0.0000,0.0000}, High: []float64{0.0000,0.0000}, Scale_factor: 5},
+		interval_test: IntervalContainer{Low: []float64{0.0,0.0}, High: []float64{1.0,1.0}, Scale_factor: 5},
 		expected: true,
 	},
-	{interval: IntervalContainer{Low: []float64{0.0,0.0}, High: []float64{0.999,0.999}, Decimal_places: 5},
-		interval_test: IntervalContainer{Low: []float64{1.0,5.0}, High: []float64{1.2,5.5}, Decimal_places: 5},
+	{interval: IntervalContainer{Low: []float64{0.0,0.0}, High: []float64{0.999,0.999}, Scale_factor: 5},
+		interval_test: IntervalContainer{Low: []float64{1.0,5.0}, High: []float64{1.2,5.5}, Scale_factor: 5},
 		expected: false,
 	}}
 
@@ -35,11 +36,14 @@ func TestInterval_ImplementsInterface(t *testing.T) {
 
 func TestCreateIntervalTree(t *testing.T){
 	assert.NotPanics(t, func(){
-		intervals := IntervalBuilder(0.0, 10.0, 1.0)
+		intervals := IntervalBuilder(0.0, 1.0, 0.1)
 		tree := NewIntervalTree(2)
+		fmt.Println(intervals)
 		for _, interval := range intervals{
 			interval := augmentedtree.Interval(interval)
 			tree.Add(interval)
 		}
+		interval := tree.Query(IntervalContainer{Id: 1, Low: []float64{0, 0}, High: []float64{0, 0}})
+		fmt.Println(interval)
 	})
 }
