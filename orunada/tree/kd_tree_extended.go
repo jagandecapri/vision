a11f581d761593a60821babcabcdb893fed4619d
Jack <jagandecapri@gmail.com>
2017-09-24 22:32:05 +0800

    Simplify KDTree_Extend struct

--- orunada/tree/kd_tree_extended.go
+++ orunada/tree/kd_tree_extended.go
@@ -2,16 +2,10 @@ package tree
 
 type KDTree_Extend struct{
 	*KDTree
-	Store map[int]*Unit
+	Units
 }
 
-func (kd_ext *KDTree_Extend) AddUnit(unit *Unit){
-	key := unit.GetID()
-	kd_ext.Store[key] = unit
-	kd_ext.Insert(unit)
-}
-
-func (kd_ext *KDTree_Extend) AddToStore(key int, p PointContainer){
-	tmp := kd_ext.Store[key]
+func (kd_ext *KDTree_Extend) AddToStore(key Range, p PointContainer){
+	tmp := kd_ext.Units[key]
 	tmp.AddPoint(p)
 }
