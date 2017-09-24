37f8887e21271f97997576292028c193c040600f
Jack <jagandecapri@gmail.com>
2017-09-24 22:30:42 +0800

    Simplify Unit struct

--- orunada/tree/unit.go
+++ orunada/tree/unit.go
@@ -5,16 +5,16 @@ import (
 )
 
 type Unit struct{
-	Id int
-	Dimension int
-	Center PointContainer
-	points map[int]PointContainer
-	recalculate_Center bool
-	*IntervalContainer
+	Id                 int
+	Cluster_id         int
+	Dimension          int
+	Center             PointContainer
+	points             map[int]PointContainer
+	recalculate_center bool
 }
 
 func (u *Unit) AddPoint(p PointContainer){
-	u.recalculate_Center = true
+	u.recalculate_center = true
 	u.points[p.GetID()] = p
 }
 
@@ -59,7 +59,7 @@ func (u *Unit) GetValue(dim int) float64{
 }
 
 func (u *Unit) Distance(p1 PointInterface) float64{
-	if u.recalculate_Center{
+	if u.recalculate_center {
 		u.RecalculateCenter()
 	}
 	sum := 0.0
@@ -74,36 +74,3 @@ func (u *Unit) Distance(p1 PointInterface) float64{
 func (u *Unit) PlaneDistance(val float64, dim int) float64{
 	return 0.0
 }
-
-
-//func (u *Unit) LowAtDimension(dim uint64) int64{
-//	return int64(u.Low[dim - 1] * math.Pow(10, float64(u.Decimal_places)))
-//}
-//
-//// HighAtDimension returns an integer representing the higher bound
-//// at the requested dimension.
-//func (u *Unit) HighAtDimension(dim uint64) int64{
-//	return int64(u.High[dim - 1] * math.Pow(10, float64(u.Decimal_places)))
-//}
-//
-//// OverlapsAtDimension should return a bool indicating if the provided
-//// interval overlaps this interval at the dimension requested.
-//func (u *Unit) OverlapsAtDimension(interval augmentedtree.Interval, dim uint64) bool{
-//	check := false
-//	for i := uint64(1); i <= uint64(len(u.Low)); i++{
-//		if interval.LowAtDimension(i) <= u.HighAtDimension(i) &&
-//			interval.HighAtDimension(i) >= u.LowAtDimension(i){
-//			check = true
-//		} else {
-//			check = false
-//		}
-//	}
-//	return check
-//}
-//
-//// ID should be a unique ID representing this interval.  This
-//// is used to identify which interval to delete from the tree if
-//// there are duplicates.
-//func (u *Unit) ID() uint64{
-//	return uint64(u.Id)
-//}
