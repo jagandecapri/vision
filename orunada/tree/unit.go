c381f280a5a5b67de92fcde8d742c30196263ff5
Jack <jagandecapri@gmail.com>
2017-09-26 14:05:21 +0000

    Store neighbour units in Unit module and add constants

--- orunada/tree/unit.go
+++ orunada/tree/unit.go
@@ -4,73 +4,76 @@ import (
 	"math"
 )
 
-type Unit struct{
+const UNCLUSTERED = 0
+const NOISE = -1
+
+type Unit struct {
 	Id                 int
 	Cluster_id         int
 	Dimension          int
 	Center             PointContainer
+	Neighbour_units    []*Unit
 	points             map[int]PointContainer
 	recalculate_center bool
 }
 
-func (u *Unit) AddPoint(p PointContainer){
+func (u *Unit) AddPoint(p PointContainer) {
 	u.recalculate_center = true
 	u.points[p.GetID()] = p
 }
 
-func (u *Unit) RemovePoint(){
+func (u *Unit) RemovePoint() {
 
 }
 
-func (u *Unit) RecalculateCenter(){
+func (u *Unit) RecalculateCenter() {
 	u.GetCenter()
 }
 
-func (u *Unit) GetCenter() PointContainer{
+func (u *Unit) GetCenter() PointContainer {
 	Center_vec := make([]float64, u.Dimension)
-	for _, p := range u.points{
-		for i := 0; i < p.Dim(); i++{
+	for _, p := range u.points {
+		for i := 0; i < p.Dim(); i++ {
 			Center_vec[i] = Center_vec[i] + p.GetValue(i)
 		}
 	}
-	for i, _ := range Center_vec{
-		Center_vec[i] = Center_vec[i]/float64(len(u.points))
+	for i, _ := range Center_vec {
+		Center_vec[i] = Center_vec[i] / float64(len(u.points))
 	}
 	u.Center = PointContainer{Unit_id: u.Id, Vec: Center_vec}
 	return u.Center
 }
 
-func (u *Unit) GetNumberOfPoints() int{
+func (u *Unit) GetNumberOfPoints() int {
 	return len(u.points)
 }
 
-
 //Implementing PointInterface methods
-func (u *Unit) GetID() int{
+func (u *Unit) GetID() int {
 	return u.Id
 }
 
-func (u *Unit) Dim() int{
+func (u *Unit) Dim() int {
 	return len(u.Center.Vec)
 }
 
-func (u *Unit) GetValue(dim int) float64{
+func (u *Unit) GetValue(dim int) float64 {
 	return u.Center.Vec[dim]
 }
 
-func (u *Unit) Distance(p1 PointInterface) float64{
+func (u *Unit) Distance(p1 PointInterface) float64 {
 	if u.recalculate_center {
 		u.RecalculateCenter()
 	}
 	sum := 0.0
 	t := p1.(*PointContainer)
-	for i:=0; i<len(u.Center.Vec); i++{
+	for i := 0; i < len(u.Center.Vec); i++ {
 		sum += math.Pow(u.Center.Vec[i]-t.Vec[i], 2)
 	}
 	euclidean_dist := math.Sqrt(sum)
 	return euclidean_dist
 }
 
-func (u *Unit) PlaneDistance(val float64, dim int) float64{
+func (u *Unit) PlaneDistance(val float64, dim int) float64 {
 	return 0.0
 }
