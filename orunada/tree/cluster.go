8cd5862bc32d437dc9e2319f32fd656ed0267783
Jack <jagandecapri@gmail.com>
2017-09-27 08:31:58 +0800

    Cluster package fixes

--- orunada/tree/cluster.go
+++ orunada/tree/cluster.go
@@ -1,19 +1,20 @@
 package tree
 
-const FIRST_CLUSTER = 1
+const UNCLASSIFIED = 0
+const NOISE = -1
 
 func Cluster(kd_tree_ext *KDTree_Extend, min_dense_points int, min_cluster_points int){
     out := make(chan PointInterface)
     kd_tree_ext.BFSTraverseChan(out)
-    cluster_id = FIRST_CLUSTER
+    cluster_id := 1
     for point := range out{
         unit := Unit(point)
-        if unit.Cluster_id == UNCLUSTERED {
-            if cluster_id == FIRST_CLUSTER{
-                unit.Cluster_id = FIRST_CLUSTER
+        if unit.Cluster_id == UNCLASSIFIED {
+            if cluster_id == 1{
+                unit.Cluster_id = 1
             } else {
-                for _, neighbour_unit := unit.Neighbour_units{
-                    if neighbour_unit != UNCLUSTERED || neighbour_unit != NOISE{
+                for _, neighbour_unit := range unit.Neighbour_units{
+                    if neighbour_unit == UNCLASSIFIED  neighbour_unit != NOISE{
                         unit.Cluster_id = neighbour_unit
                     }
                 }
