da16b00d99a98430f6a6b9dfba96dbf9d3bb543c
Jack <jagandecapri@gmail.com>
2017-10-03 22:33:12 +0800

    Working on cluster.go

--- orunada/tree/cluster.go
+++ orunada/tree/cluster.go
@@ -6,25 +6,24 @@ const NOISE = -1
 const CORE_UNIT = 1
 const NOT_CORE_UNIT = 2
 
-func Cluster(kd_tree_ext *KDTree_Extend, min_dense_points int, min_cluster_points int) {
-	out := make(chan PointInterface)
-	kd_tree_ext.BFSTraverseChan(out)
+var cluster_map map[int]Unit
+
+func Cluster(units Units, min_dense_points int, min_cluster_points int) {
 	cluster_id := 1
-	for point := range out {
-		unit := Unit(point)
+	for rg, unit := range units.Store {
 		seeds := Queue{}
-		if isDenseUnit(unit, min_dense_points) {
+		if isDenseUnit(unit, min_dense_points) == true {
 			unit.Cluster_id = cluster_id
 			for _, neighbour_unit := range unit.Neighbour_units {
 				if neighbour_unit.Cluster_id == UNCLASSIFIED || neighbour_unit.Cluster_id == NOISE {
 					if neighbour_unit.GetNumberOfPoints() >= min_dense_points {
 						neighbour_unit.Cluster_id = cluster_id
-						seeds.push(neighbour_unit)
+						seeds.Push(neighbour_unit)
 					}
 				}
 			}
 			if len(seeds) == 0 {
-				unit.Cluster_id == NOISE
+				unit.Cluster_id = NOISE
 			} else {
 				for seed := seeds.Pop(); seed != nil; {
 					expandCluster(seed, cluster_id, min_dense_points, seeds)
@@ -32,6 +31,7 @@ func Cluster(kd_tree_ext *KDTree_Extend, min_dense_points int, min_cluster_point
 			}
 		}
 		cluster_id++
+                units.Store[rg] = unit
 	}
 }
 
@@ -41,11 +41,11 @@ func expandCluster(unit *Unit, cluster_id int, min_dense_points int, seeds Queue
 			unit.Cluster_id = cluster_id
 		}
 		if isDenseUnit(neighbour_unit, min_dense_points) {
-			seeds.push(neighbour_unit)
+			seeds.Push(neighbour_unit)
 		}
 	}
 }
 
-func isDenseUnit(unit *Unit, min_dense_points int) {
+func isDenseUnit(unit *Unit, min_dense_points int) bool{
 	return unit.GetNumberOfPoints() >= min_dense_points
 }
