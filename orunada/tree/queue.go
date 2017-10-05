ef9877a7ea50b440d94a1cc3b2b45f197a7ccd25
Jack <jagandecapri@gmail.com>
2017-10-05 23:22:45 +0800

    Add queue for Unit struct type

--- orunada/tree/queue.go
+++ orunada/tree/queue.go
@@ -18,3 +18,20 @@ func (q *Queue) Len() int {
 	return len(*q)
 }
 
+type QueueUnit []*Unit
+
+func (q *QueueUnit) Push(n *Unit) {
+	*q = append(*q, n)
+}
+
+func (q *QueueUnit) Pop() (n *Unit) {
+	if len := q.Len(); len > 0 {
+		n = (*q)[0]
+		*q = append(QueueUnit(nil), (*q)[1:]...)
+	}
+	return n
+}
+
+func (q *QueueUnit) Len() int {
+	return len(*q)
+}
