b48a58d080880612bf986b71df6b9734a5d0b84b
Jack <jagandecapri@gmail.com>
2017-10-09 08:34:01 +0800

    Fix bug in cluster_test

--- orunada/tree/cluster_test.go
+++ orunada/tree/cluster_test.go
@@ -44,44 +44,44 @@ func TestCluster3by3Grid(t *testing.T) {
 	units := Units{Store: make(map[Range]*Unit)}
 	interval_l := 1.0
 
-	u1 := Unit{Id: 1, Center: PointContainer{Vec: []float64{0.5,0,5}},
+	u1 := Unit{Id: 1, Center: PointContainer{Vec: []float64{0.5,0.5}},
 		points: map[int]PointContainer{1: {},2: {},3: {},4: {},5: {}}}
 	r1 := Range{Low: [2]float64{0, 0}, High: [2]float64{1, 1}}
 	units.AddUnit(&u1, r1)
 
-	u2 := Unit{Id: 2, Center: PointContainer{Vec: []float64{0.5,0,5}},
+	u2 := Unit{Id: 2, Center: PointContainer{Vec: []float64{0.5,0.5}},
 		points: map[int]PointContainer{1: {},2: {}}}
 	r2 := Range{Low: [2]float64{0, 1}, High: [2]float64{1, 2}}
 	units.AddUnit(&u2, r2)
 
-	u3 := Unit{Id: 3, Center: PointContainer{Vec: []float64{0.5,2,5}},
+	u3 := Unit{Id: 3, Center: PointContainer{Vec: []float64{0.5,2.5}},
 		points: map[int]PointContainer{1: {},2: {},3: {}}}
 	r3 := Range{Low: [2]float64{0, 2}, High: [2]float64{1, 3}}
 	units.AddUnit(&u3, r3)
 
-	u4 := Unit{Id: 4, Center: PointContainer{Vec: []float64{1.5,0,5}},
+	u4 := Unit{Id: 4, Center: PointContainer{Vec: []float64{1.5,0.5}},
 		points: map[int]PointContainer{1: {}}}
 	r4 := Range{Low: [2]float64{1, 0}, High: [2]float64{2, 1}}
 	units.AddUnit(&u4, r4)
 
-	u5 := Unit{Id: 5, Center: PointContainer{Vec: []float64{1.5,1,5}}}
+	u5 := Unit{Id: 5, Center: PointContainer{Vec: []float64{1.5,1.5}}}
 	r5 := Range{Low: [2]float64{1, 1}, High: [2]float64{2, 2}}
 	units.AddUnit(&u5, r5)
 
-	u6 := Unit{Id: 6, Center: PointContainer{Vec: []float64{1.5,2,5}}}
+	u6 := Unit{Id: 6, Center: PointContainer{Vec: []float64{1.5,2.5}}}
 	r6 := Range{Low: [2]float64{1, 2}, High: [2]float64{2, 3}}
 	units.AddUnit(&u6, r6)
 
-	u7 := Unit{Id: 7, Center: PointContainer{Vec: []float64{2.5,0,5}}}
+	u7 := Unit{Id: 7, Center: PointContainer{Vec: []float64{2.5,0.5}}}
 	r7 := Range{Low: [2]float64{2, 0}, High: [2]float64{3, 1}}
 	units.AddUnit(&u7, r7)
 
-	u8 := Unit{Id: 8, Center: PointContainer{Vec: []float64{2.5,1,5}},
+	u8 := Unit{Id: 8, Center: PointContainer{Vec: []float64{2.5,1.5}},
 		points: map[int]PointContainer{1: {},2: {}, 3: {},4: {}}}
 	r8 := Range{Low: [2]float64{2, 1}, High: [2]float64{3, 2}}
 	units.AddUnit(&u8, r8)
 
-	u9 := Unit{Id: 9, Center: PointContainer{Vec: []float64{0.5,0,5}},
+	u9 := Unit{Id: 9, Center: PointContainer{Vec: []float64{0.5,0.5}},
 		points: map[int]PointContainer{1: {}}}
 	r9 := Range{Low: [2]float64{2, 2}, High: [2]float64{3, 3}}
 	units.AddUnit(&u9, r9)
