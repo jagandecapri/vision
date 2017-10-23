2b448cdcc890e464408b6da8af6cb7d1e9f83889
Jack <jagandecapri@gmail.com>
2017-10-23 21:38:26 +0800

    Add test for ProcessOldDenseUnits

--- orunada/tree/units_test.go
+++ orunada/tree/units_test.go
@@ -155,3 +155,34 @@ func TestUnits_RecomputeDenseUnits(t *testing.T) {
 	_, ok = units.listOldDenseUnits[rg1]
 	assert.False(t, ok)
 }
+
+func TestUnits_ProcessOldDenseUnits(t *testing.T) {
+	unit := NewUnit(1, 2)
+	unit.Id = 9
+	unit.Cluster_id = 1
+	unit.Neighbour_units = map[Range]*Unit{{Low: [2]float64{0, 0}, High: [2]float64{1, 1}}: {Id: 1, Cluster_id: 1},
+		{Low: [2]float64{0, 1}, High: [2]float64{1, 2}}: {Id: 2, Cluster_id: 1},
+		{Low: [2]float64{0, 2}, High: [2]float64{1, 3}}: {Id: 3},
+		{Low: [2]float64{1, 0}, High: [2]float64{2, 1}}: {Id: 4},
+		{Low: [2]float64{1, 2}, High: [2]float64{2, 3}}: {Id: 5},
+		{Low: [2]float64{2, 0}, High: [2]float64{3, 1}}: {Id: 6},
+		{Low: [2]float64{2, 1}, High: [2]float64{3, 2}}: {Id: 7},
+		{Low: [2]float64{2, 2}, High: [2]float64{3, 3}}: {Id: 8}}
+	units := NewUnits()
+	list_of_units := map[Range]*Unit{
+		Range{Low: [2]float64{0, 0}, High: [2]float64{1, 1}}: {Id: 1, Cluster_id: 1},
+		Range{Low: [2]float64{0, 1}, High: [2]float64{1, 2}}: {Id: 2, Cluster_id: 1},
+	}
+	units.Cluster_map = map[int]Cluster{1: {ListOfUnits: list_of_units}}
+	units.listOldDenseUnits = map[Range]*Unit{{Low: [2]float64{1, 1}, High: [2]float64{2, 2}}: &unit}
+
+	units.ProcessOldDenseUnits()
+
+	unit_ids := []int{}
+	for _, nu := range units.tmpUnitToCluster{
+		unit_ids = append(unit_ids, nu.Id)
+	}
+
+	assert.Contains(t, unit_ids, 1)
+	assert.Contains(t, unit_ids, 2)
+}
