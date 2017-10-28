0b61f89e1d514a7e8f1dd4d0ad4129c70df90a36
Jack <jagandecapri@gmail.com>
2017-10-28 17:07:46 +0800

    Added test for absorbing and merging cluster, done IGDCA

--- orunada/tree/cluster.go
+++ orunada/tree/cluster.go
@@ -1,6 +1,8 @@
 package tree
 
-import "github.com/jagandecapri/vision/orunada/utils"
+import (
+	"github.com/jagandecapri/vision/orunada/utils"
+)
 
 const UNCLASSIFIED = 0
 const NOISE = -1
@@ -32,14 +34,14 @@ func GDA(Units Units, min_dense_points int, min_cluster_points int) (map[Range]*
 	for rg, unit := range units{
 		if unit.Cluster_id == UNCLASSIFIED{
 			if isDenseUnit(unit, min_dense_points){ //TODO: Could be redundant is only dense units are sent
-				var ret bool
+				var ret int
 				var neighbour_cluster_ids []int
-				ret, neighbour_cluster_ids = AbsorbIntoCluster(unit, Units, min_dense_points)
+				ret, neighbour_cluster_ids = AbsorbIntoCluster(unit, min_dense_points)
 				if ret == SUCCESS{
 					if len(neighbour_cluster_ids) > 1{
-						MergeClusters(units, neighbour_cluster_ids)
+						MergeClusters(cluster_map, neighbour_cluster_ids)
 					}
-				} else if ret == FAILURE{
+				}else if ret == FAILURE{
 					NewCluster(unit, rg, cluster_id, min_dense_points, min_cluster_points, cluster_map)
 					cluster_id = Units.GetNextClusterID()
 				}
@@ -60,11 +62,11 @@ func NewCluster(unit *Unit, rg Range, cluster_id int, min_dense_points int, min_
 	cluster_map[cluster_id] = cluster
 }
 
-func AbsorbIntoCluster(unit *Unit, units Units, min_dense_points int) (int, neighbour_cluster_ids []int){
+func AbsorbIntoCluster(unit *Unit, min_dense_points int) (int, []int){
 	ret_value := FAILURE
 	cluster_ids := []int{}
 	for _, neighbour_unit := range unit.Neighbour_units{
-		if isDenseUnit(neighbour_unit, min_dense_points) && neighbour_unit.Cluster_id != UNCLASSIFIED ||
+		if isDenseUnit(neighbour_unit, min_dense_points) && neighbour_unit.Cluster_id != UNCLASSIFIED &&
 			neighbour_unit.Cluster_id != NOISE{
 			unit.Cluster_id = neighbour_unit.Cluster_id
 			cluster_ids = append(cluster_ids, unit.Cluster_id)
@@ -80,9 +82,8 @@ func AbsorbIntoCluster(unit *Unit, units Units, min_dense_points int) (int, neig
 	return ret_value, cluster_ids
 }
 
-func MergeClusters(units Units, cluster_ids []int) int{
+func MergeClusters(cluster_map map[int]Cluster, cluster_ids []int) int{
 	ret_value := FAILURE
-	cluster_map := units.GetClusterMap()
 	cluster_id_to_merge := cluster_ids[0]
 	for _, cluster_id := range cluster_ids{
 		cluster, ok := cluster_map[cluster_id]
--- orunada/tree/cluster_test.go
+++ orunada/tree/cluster_test.go
@@ -48,11 +48,105 @@ func TestCluster2by2Grid(t *testing.T) {
 	min_cluster_points := 5
 
 	res, cluster_map := GDA(units, min_dense_points, min_cluster_points)
-	assert.True(t, res[r1].Cluster_id == res[r2].Cluster_id,
-		"%v %v %v", res[r1].Cluster_id, res[r2].Cluster_id)
+	assert.True(t, res[r1].Cluster_id == res[r2].Cluster_id && res[r2].Cluster_id == res[r3].Cluster_id,
+		"%v %v %v", res[r1].Cluster_id, res[r2].Cluster_id, res[r3].Cluster_id)
 	outlier_cluster_count, non_outlier_cluster_count := CountClusterTypes(cluster_map)
 	assert.Equal(t, 0, outlier_cluster_count)
 	assert.Equal(t, 1, non_outlier_cluster_count)
+}
+
+func TestCluster2by2GridAbsorbCluster(t *testing.T) {
+	units := NewUnits()
+	interval_l := 1.0
+
+	u1 := Unit{Id: 1, Center: PointContainer{Vec: []float64{0.5,0.5}},
+		points: map[int]PointContainer{1: {},2: {},3: {},4: {},5: {}}}
+	r1 := Range{Low: [2]float64{0, 0}, High: [2]float64{1, 1}}
+	units.AddUnit(&u1, r1)
+
+	u2 := Unit{Id: 2, Cluster_id: 1, Center: PointContainer{Vec: []float64{0.5,1.5}},
+		points: map[int]PointContainer{1: {},2: {}}}
+	r2 := Range{Low: [2]float64{0, 1}, High: [2]float64{1, 2}}
+	units.AddUnit(&u2, r2)
+
+	u3 := Unit{Id: 3, Cluster_id: 1, Center: PointContainer{Vec: []float64{1.5,0.5}},
+		points: map[int]PointContainer{1: {},2: {}}}
+	r3 := Range{Low: [2]float64{1, 0}, High: [2]float64{2, 1}}
+	units.AddUnit(&u3, r3)
+
+	u4 := Unit{Id: 4, Center: PointContainer{Vec: []float64{1.5,1.5}}}
+	r4 := Range{Low: [2]float64{1, 1}, High: [2]float64{2, 2}}
+	units.AddUnit(&u4, r4)
+
+	units.SetupGrid(interval_l)
+
+	min_dense_points := 2
+
+	res, cluster_ids := AbsorbIntoCluster(&u1, min_dense_points)
+	assert.Equal(t, res, SUCCESS)
+	assert.Equal(t, 1, len(cluster_ids), "%v", cluster_ids)
+	assert.Contains(t, cluster_ids, 1)
+	assert.Equal(t, 1, u1.Cluster_id)
+}
+
+func TestCluster2by2GridMergeClusters(t *testing.T) {
+	units := NewUnits()
+	interval_l := 1.0
+
+	u1 := Unit{Id: 1, Center: PointContainer{Vec: []float64{0.5,0.5}},
+		points: map[int]PointContainer{1: {},2: {},3: {},4: {},5: {}}}
+	r1 := Range{Low: [2]float64{0, 0}, High: [2]float64{1, 1}}
+	units.AddUnit(&u1, r1)
+
+	u2 := Unit{Id: 2, Cluster_id: 1, Center: PointContainer{Vec: []float64{0.5,1.5}},
+		points: map[int]PointContainer{1: {},2: {}}}
+	r2 := Range{Low: [2]float64{0, 1}, High: [2]float64{1, 2}}
+	units.AddUnit(&u2, r2)
+
+	u3 := Unit{Id: 3, Cluster_id: 2, Center: PointContainer{Vec: []float64{1.5,0.5}},
+		points: map[int]PointContainer{1: {},2: {}}}
+	r3 := Range{Low: [2]float64{1, 0}, High: [2]float64{2, 1}}
+	units.AddUnit(&u3, r3)
+
+	u4 := Unit{Id: 4, Center: PointContainer{Vec: []float64{1.5,1.5}}}
+	r4 := Range{Low: [2]float64{1, 1}, High: [2]float64{2, 2}}
+	units.AddUnit(&u4, r4)
+
+	units.SetupGrid(interval_l)
+
+	units.Cluster_map = map[int]Cluster{
+		1: {
+			Cluster_id: 1,
+			ListOfUnits: map[Range]*Unit{
+				r1: &u1,
+				r2: &u2,
+			},
+		},
+		2: {
+			Cluster_id: 2,
+			ListOfUnits: map[Range]*Unit{
+				r3: &u3,
+			},
+		},
+	}
+
+	var cluster_ids []int
+	var res int
+	cluster_ids = []int{1,2}
+	res = MergeClusters(units.Cluster_map,cluster_ids)
+	assert.Equal(t, res, SUCCESS)
+	assert.Equal(t, 1, u1.Cluster_id)
+	assert.Equal(t, 1, u2.Cluster_id)
+	assert.Equal(t, 1, u3.Cluster_id)
+	assert.Equal(t, UNCLASSIFIED, u4.Cluster_id)
+
+	cluster_ids = []int{2,1}
+	res = MergeClusters(units.Cluster_map,cluster_ids)
+	assert.Equal(t, res, SUCCESS)
+	assert.Equal(t, 2, u1.Cluster_id)
+	assert.Equal(t, 2, u2.Cluster_id)
+	assert.Equal(t, 2, u3.Cluster_id)
+	assert.Equal(t, UNCLASSIFIED, u4.Cluster_id)
 }
 
 func TestCluster3by3Grid(t *testing.T) {
