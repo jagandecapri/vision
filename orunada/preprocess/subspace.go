67ade8498651e1ea2e4be8cef81a577f7b42a6c5
Jack <jagandecapri@gmail.com>
2017-11-04 18:22:09 +0800

    Add subspace module with basic testings

--- orunada/preprocess/subspace.go
+++ orunada/preprocess/subspace.go
@@ -1,1 +1,40 @@
 package preprocess
+
+import (
+	"github.com/jagandecapri/vision/orunada/tree"
+	"github.com/Workiva/go-datastructures/augmentedtree"
+)
+
+type Subspace struct{
+	Interval_tree *augmentedtree.Tree
+	Units *tree.Units
+	Subspace_key [2]string
+	Scale_factor int
+}
+
+func (s *Subspace) GetSubspace(mat []tree.Point) {
+	subspace_key := s.Subspace_key
+	for _, p := range mat{
+		tmp := [2]float64{p.Vec_map[subspace_key[0]], p.Vec_map[subspace_key[1]]}
+		tmp1 := [2]float64{p.Vec_map[subspace_key[0]], p.Vec_map[subspace_key[1]]}
+		int_container := tree.IntervalContainer{Id: 1, Range: tree.Range{Low: tmp, High: tmp1}, Scale_factor: s.Scale_factor}
+		interval := (*s.Interval_tree).Query(int_container)
+		if len(interval) > 0{
+			interval_ext := interval[0].(tree.IntervalContainer)
+			Vec := []float64{p.Vec_map[subspace_key[0]], p.Vec_map[subspace_key[1]]}
+			pnt_container := tree.PointContainer{
+				Unit_id:  int(interval[0].ID()),
+				Vec: Vec,
+				Point: p,
+			}
+			cur_rg := s.Units.GetPointRange(pnt_container.GetID())
+			new_rg := interval_ext.Range
+			if cur_rg == (tree.Range{}){
+				s.Units.AddPoint(pnt_container, new_rg)
+			} else if cur_rg != (tree.Range{}) && cur_rg != new_rg{
+				s.Units.UpdatePoint(pnt_container, new_rg)
+			}
+		} else {
+		}
+	}
+}
---
+++ orunada/preprocess/subspace_test.go
@@ -0,0 +1,72 @@
+package preprocess
+
+import (
+	"testing"
+	"github.com/jagandecapri/vision/orunada/tree"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSubspace_GetSubspace(t *testing.T) {
+	subspace_key := [2]string{"first", "second"}
+	min_interval := 0.0
+	max_interval := 0.2
+	interval_length := 0.1
+	dim := 2
+	scale_factor := 5
+	ranges := tree.RangeBuilder(min_interval, max_interval, interval_length)
+	intervals := tree.IntervalBuilder(ranges, scale_factor)
+	units := tree.UnitsBuilder(ranges, dim)
+
+	int_tree := tree.NewIntervalTree(uint64(dim))
+	Unit := tree.NewUnits()
+
+	for _, interval := range intervals{
+		int_tree.Add(interval)
+	}
+
+	for rg, unit := range units{
+		Unit.AddUnit(&unit, rg)
+	}
+
+	subspace := Subspace{Interval_tree: &int_tree,
+		Units: &Unit,
+		Subspace_key: subspace_key,
+		Scale_factor: scale_factor,
+	}
+
+	//To test new points
+	p1 := tree.Point{Id: 1, Vec_map: map[string]float64{
+		"first": 0.05,
+		"second": 0.05,
+		"three": 0.15,
+	}}
+	p2 := tree.Point{Id: 2, Vec_map: map[string]float64{
+		"first": 0.15,
+		"second": 0.15,
+		"three": 0.15,
+	}}
+
+	points := []tree.Point{p1, p2}
+	subspace.GetSubspace(points)
+
+	assert.Equal(t, tree.Range{Low: [2]float64{0, 0}, High: [2]float64{0.1, 0.1}}, subspace.Units.Point_unit_map[1])
+	assert.Equal(t, tree.Range{Low: [2]float64{0.1, 0.1}, High: [2]float64{0.2, 0.2}}, subspace.Units.Point_unit_map[2])
+
+	//To test updating points
+	p1 = tree.Point{Id: 1, Vec_map: map[string]float64{
+		"first": 0.15,
+		"second": 0.15,
+		"three": 0.15,
+	}}
+	p2 = tree.Point{Id: 2, Vec_map: map[string]float64{
+		"first": 0.05,
+		"second": 0.05,
+		"three": 0.15,
+	}}
+
+	points = []tree.Point{p1, p2}
+	subspace.GetSubspace(points)
+
+	assert.Equal(t, tree.Range{Low: [2]float64{0.1, 0.1}, High: [2]float64{0.2, 0.2}}, subspace.Units.Point_unit_map[1])
+	assert.Equal(t, tree.Range{Low: [2]float64{0, 0}, High: [2]float64{0.1, 0.1}}, subspace.Units.Point_unit_map[2])
+}
