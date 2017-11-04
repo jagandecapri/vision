a9aa30ccf21cbefab2782bf68e52b21bc1b30163
Jack <jagandecapri@gmail.com>
2017-11-04 21:09:37 +0800

    Extract normalization into own module

---
+++ orunada/preprocess/normalize.go
@@ -0,0 +1,59 @@
+package preprocess
+
+import "github.com/jagandecapri/vision/orunada/tree"
+
+type DimMinMax struct{
+	Min, Max float64
+	Range float64
+}
+
+func norm_mat(elem float64, col_min float64, col_max float64) float64{
+	return (elem - col_min)/(col_max - col_min)
+}
+
+func Normalize(mat []tree.Point, sorter []string) ([]tree.Point, map[string]DimMinMax){
+	rows := len(mat)
+
+	dim_min_max := map[string]DimMinMax{}
+	for _,c := range sorter {
+		min := mat[0].Vec_map[c]
+		max := mat[0].Vec_map[c]
+		for j := 0; j < rows; j++{
+			val := mat[j].Vec_map[c]
+			if val < min{
+				min = val
+			} else if  val > max{
+				max = val
+			}
+		}
+		range_ := max - min
+		dim_min_max[c] = DimMinMax{min, max, range_}
+	}
+
+	for i := 0; i < rows; i++{
+		for _, c := range sorter{
+			col_min := dim_min_max[c].Min
+			col_max := dim_min_max[c].Max
+			elem := mat[i].Vec_map[c]
+			if col_min == 0 && col_max == 0{
+				//mat[i].Vec_map[c] = scale(elem, float64(scale_factor))
+				mat[i].Vec_map[c] = elem
+			} else {
+				//mat[i].Vec_map[c] = scale(norm_mat(elem, col_min, col_max), float64(scale_factor)) //(elem - col_min)/(col_max - col_min)
+				mat[i].Vec_map[c] = norm_mat(elem, col_min, col_max) //(elem - col_min)/(col_max - col_min)
+			}
+		}
+	}
+
+	//Assign normalized min-max
+	for _,c := range sorter{
+		norm_col_min := 0.0
+		tmp := dim_min_max[c]
+		tmp.Min = norm_col_min
+		norm_col_max := 1.0
+		tmp.Max = norm_col_max
+		dim_min_max[c] = tmp
+	}
+
+	return mat, dim_min_max
+}
