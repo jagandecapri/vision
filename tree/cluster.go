07cb0d5b3b642d106114b25c5d8836999a63b48a
Jack <jagandecapri@gmail.com>
2018-01-31 21:46:13 +0800

    Add method to get center of cluster

--- tree/cluster.go
+++ tree/cluster.go
@@ -6,3 +6,25 @@ type Cluster struct{
 	Num_of_points int
 	ListOfUnits map[Range]*Unit
 }
+
+func (c *Cluster) GetCenter() PointContainer{
+	var Center_vec []float64
+
+	for _, unit := range c.ListOfUnits{
+		if Center_vec == nil{
+			Center_vec = make([]float64, unit.Dim())
+		}
+		for _, p := range unit.Points {
+			for i := 0; i < p.Dim(); i++ {
+				Center_vec[i] = Center_vec[i] + p.GetValue(i)
+			}
+		}
+	}
+
+	for i, _ := range Center_vec {
+		Center_vec[i] = Center_vec[i] / float64(c.Num_of_points)
+	}
+
+	pc := PointContainer{Vec: Center_vec}
+	return pc
+}
---
+++ tree/cluster_test.go
@@ -0,0 +1,36 @@
+package tree
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCluster_GetCenter(t *testing.T) {
+
+	r1 := Range{Low: [2]float64{0, 0}, High: [2]float64{1, 1}}
+	u1 := Unit{Id: 1, Center: PointContainer{Vec: []float64{(0.1 + 0.2 + 0.3 + 0.4 + 0.5)/5, (0.1 + 0.2 + 0.3 + 0.4 + 0.5)/5}},
+	Points: map[int]PointContainer{1: {Vec: []float64{0.1, 0.1}},
+	2: {Vec: []float64{0.2, 0.2}},
+	3: {Vec: []float64{0.3, 0.3}},
+	4: {Vec: []float64{0.4, 0.4}},
+	5: {Vec: []float64{0.5, 0.5}}},
+	Range: r1}
+
+	r2 := Range{Low: [2]float64{1, 1}, High: [2]float64{2, 2}}
+	u2 := Unit{Id: 2, Center: PointContainer{Vec: []float64{(1.1 + 1.2 + 1.3 + 1.4)/4, (1.1 + 1.2 + 1.3 + 1.4)/4}},
+	Points: map[int]PointContainer{1: {Vec: []float64{1.1, 1.1}},
+	2: {Vec: []float64{1.2, 1.2}},
+	3: {Vec: []float64{1.3, 1.3}},
+	4: {Vec: []float64{1.4, 1.4}}},
+	Range: r2}
+
+	cluster := Cluster{Num_of_points: 9, ListOfUnits: map[Range]*Unit{r1: &u1, r2: &u2}}
+
+	x1 := (0.1 + 0.2 + 0.3 + 0.4 + 0.5 + 1.1 + 1.2 + 1.3 + 1.4)/9
+	x2 := (0.1 + 0.2 + 0.3 + 0.4 + 0.5 + 1.1 + 1.2 + 1.3 + 1.4)/9
+	center := PointContainer{Vec: []float64{x1, x2}}
+
+	pc := cluster.GetCenter()
+
+	assert.Equal(t, center, pc)
+}
