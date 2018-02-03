873214c64a74b991bff31aa09cf8f593b8b9863a
Jack <jagandecapri@gmail.com>
2018-02-03 17:06:40 +0800

    Remove PointInterface implementation in Unit

--- tree/unit.go
+++ tree/unit.go
@@ -1,9 +1,5 @@
 package tree
 
-import (
-	"math"
-)
-
 type Unit struct {
 	Id                int
 	Cluster_id        int
@@ -63,34 +59,12 @@ func (u *Unit) GetNumberOfPoints() int {
 	return len(u.Points)
 }
 
-//Implementing PointInterface methods
 func (u *Unit) GetID() int {
 	return u.Id
 }
 
 func (u *Unit) Dim() int {
 	return len(u.Center.Vec)
-}
-
-func (u *Unit) GetValue(dim int) float64 {
-	return u.Center.Vec[dim]
-}
-
-func (u *Unit) Distance(p1 PointInterface) float64 {
-	if u.Center_calculated {
-		u.CalculateCenter()
-	}
-	sum := 0.0
-	t := p1.(*Point)
-	for i := 0; i < len(u.Center.Vec); i++ {
-		sum += math.Pow(u.Center.Vec[i]-t.Vec[i], 2)
-	}
-	euclidean_dist := math.Sqrt(sum)
-	return euclidean_dist
-}
-
-func (u *Unit) PlaneDistance(val float64, dim int) float64 {
-	return 0.0
 }
 
 func NewUnit(id int, dimension int, rg Range) Unit{
