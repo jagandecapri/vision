453531510fcfc206ec14c17f5aa2e2f5866fc61f
Jack <jagandecapri@gmail.com>
2018-02-07 21:44:56 +0800

    Remove GetVisualizationData method

--- process/process_data.go
+++ process/process_data.go
@@ -6,23 +6,6 @@ import (
 	"github.com/jagandecapri/vision/server"
 )
 
-func GetVisualizationData(subspace tree.Subspace) (string, server.PointCluster){
-
-	grid := subspace.Grid
-	key := strings.Join(subspace.Subspace_key[:], "-")
-	point_cluster := server.PointCluster{}
-	for _, unit := range grid.Store{
-		cluster_id := unit.Cluster_id
-		tmp := [][]float64{}
-		for _, point_container := range unit.Points{
-			tmp = append(tmp, point_container.Vec)
-		}
-		point_cluster[cluster_id] = append(point_cluster[cluster_id], tmp...)
-	}
-
-	return key, point_cluster
-}
-
 func ProcessDataForVisualization(subspaces []tree.Subspace) server.HttpData1{
 	graphs := server.HttpData1{}
 
