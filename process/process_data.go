341492c246e9d9a5dcc809e89bb63f4b5ce31f26
Jack <jagandecapri@gmail.com>
2018-02-10 13:34:25 +0800

    Refactor process data module

--- process/process_data.go
+++ process/process_data.go
@@ -6,6 +6,39 @@ import (
 	"github.com/jagandecapri/vision/server"
 )
 
+func GetClusteredUnitPoints(cluster tree.Cluster) server.Points{
+
+	units := cluster.GetUnits()
+	tmp := server.Points{}
+
+	for _, unit := range units{
+		points := unit.GetPoints()
+
+		tmp1 := []server.Point{}
+
+		for _, point := range points{
+			X := point.GetValue(0)
+			Y := point.GetValue(1)
+
+			point_data := server.Point{
+				Point_data: server.Point_data{
+					X: X,
+					Y: Y,
+				},
+			}
+
+			tmp1 = append(tmp1, point_data)
+		}
+
+		tmp.Point_list = tmp1
+		tmp.Point_metadata = server.Point_metadata{
+			Color: "#ABC",
+		}
+	}
+
+	return tmp
+}
+
 func ProcessDataForVisualization(subspaces []tree.Subspace) server.HttpData{
 	graphs := server.HttpData{}
 
@@ -19,45 +52,16 @@ func ProcessDataForVisualization(subspaces []tree.Subspace) server.HttpData{
 			},
 		}
 
-		 clusters := subspace.GetClusters()
-
 		points_data := []server.Points{}
 
-		 for _, cluster := range clusters{
-		 	units := cluster.GetUnits()
-
-		 	tmp := server.Points{}
-		 	for _, unit := range units{
-		 		points := unit.GetPoints()
-
-				tmp1 := []server.Point{}
-
-		 		for _, point := range points{
-		 			X := point.GetValue(0)
-		 			Y := point.GetValue(1)
-
-		 			point_data := server.Point{
-		 				Point_data: server.Point_data{
-							X: X,
-							Y: Y,
-						},
-					}
-
-					tmp1 = append(tmp1, point_data)
-				}
-
-				tmp.Point_list = tmp1
-				tmp.Point_metadata = server.Point_metadata{
-					Color: "#ABC",
-				}
-			}
-
+		clusters := subspace.GetClusters()
+		for _, cluster := range clusters{
+			tmp := GetClusteredUnitPoints(cluster)
 			points_data = append(points_data, tmp)
-		 }
+		}
 
-		 graph.Points = points_data
-
-		 graphs = append(graphs, graph)
+		graph.Points = points_data
+		graphs = append(graphs, graph)
 	}
 
 	return graphs
