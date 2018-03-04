8e73cc56d7cf5a8766f102d025f15f1151433ff8
Jack <jagandecapri@gmail.com>
2018-03-04 17:00:29 +0800

    Dissimilarity map calculates for one subspace

--- process/ea40.go
+++ process/ea40.go
@@ -2,22 +2,19 @@ package process
 
 import (
 	"github.com/jagandecapri/vision/tree"
-	"sort"
 )
 
-func ComputeDissmilarityVector(subspaces map[[2]string]tree.Subspace){
-	dissimilarity_map := map[int]float64{}
-	for subspace_name, subspace := range subspaces{
-		center_biggest_cluster := subspace.GetBiggestCluster().GetCenter()
-		outlier_clusters := subspace.GetOutliers()
-		for _, cluster := range outlier_clusters{
-			for _, unit := range cluster.ListOfUnits{
-				for id, point := range unit.Points{
-					dissimilarity_map[id] = dissimilarity_map[id] + center_biggest_cluster.Distance(&point)
-				}
-			}
-		}
+func ComputeDissmilarityVector(subspace tree.Subspace) map[tree.PointKey]float64{
+	dissimilarity_map := map[tree.PointKey]float64{}
+
+	outliers := subspace.GetOutliers()
+	cluster := subspace.GetBiggestCluster()
+	center_biggest_cluster := cluster.GetCenter()
+
+	for _, outlier := range outliers{
+		distance := center_biggest_cluster.Distance(&outlier)
+		dissimilarity_map[outlier.Key] = distance
 	}
 
-	sort
+	return dissimilarity_map
 }
