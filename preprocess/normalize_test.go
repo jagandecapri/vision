c4aecf9315bca35438723a4899a37c28fd272a7d
Jack <jagandecapri@gmail.com>
2018-03-04 15:50:58 +0800

    Add test for normalization

--- preprocess/normalize_test.go
+++ preprocess/normalize_test.go
@@ -40,3 +40,39 @@ func TestNormalize(t *testing.T) {
 	norm_points := Normalize(points, sorter)
 	assert.Equal(t, normalized_points, norm_points)
 }
+
+func TestNormalize2(t *testing.T) {
+	points := []tree.Point{{Id: 1, Vec_map: map[string]float64{
+		"first": 5,
+		"second": 10,
+	}},
+		{Id: 2, Vec_map: map[string]float64{
+			"first": 0,
+			"second": 6,
+		}},
+		{Id: 3, Vec_map: map[string]float64{
+			"first": 3,
+			"second": 7,
+		}},
+	}
+
+	normalized_points := []tree.Point{{Id: 1, Vec_map: map[string]float64{
+		"first": 0.9999999,
+		"second": 0.9999999,
+	}},
+		{Id: 2, Vec_map: map[string]float64{
+			"first": 0,
+			"second": 0,
+		}},
+		{Id: 3, Vec_map: map[string]float64{
+			"first": 0.6,
+			"second": 0.25,
+		}},
+	}
+
+	sorter := []string{"first", "second"}
+
+	norm_points := Normalize(points, sorter)
+	assert.Equal(t, normalized_points, norm_points)
+}
+
