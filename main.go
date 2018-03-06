10b0dc76b5525f2caf4e3c5a5fe12e0a1a3d142c
Jack <jagandecapri@gmail.com>
2018-03-06 23:07:23 +0800

    Integrate refactoring for agg levels in main.go

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/jagandecapri/vision/utils"
 	"github.com/jagandecapri/vision/preprocess"
 	"github.com/jagandecapri/vision/process"
 	"github.com/google/gopacket/pcap"
@@ -13,6 +12,8 @@ import (
 	"github.com/jagandecapri/vision/tree"
 	"github.com/jagandecapri/vision/server"
 	"flag"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"github.com/jagandecapri/vision/anomalies"
 )
 
 var scale_factor = 5
@@ -20,56 +21,35 @@ var scale_factor = 5
 func getSorter() []string{
 	sorter := []string{}
 	sorter = append(sorter, "nbPacket", "nbSrcPort", "nbDstPort",
-		"nbSrcs", "nbDsts", "perSyn", "perAck", "perRST", "perFIN",
-			"perCWR", "perURG", "avgPktSize", "meanTTL")
+		"nbSrcs", "nbDsts", "perSYN", "perACK", "perRST", "perFIN",
+			"perCWR", "perURG", "perICMP", "avgPktSize", "meanTTL")
 	sort.Strings(sorter)
 	return sorter
 }
 
 func main(){
+	log.SetOutput(&lumberjack.Logger{
+		Filename:   "C:\\Users\\Jack\\go\\src\\github.com\\jagandecapri\\vision\\logs\\lumber_log.log",
+		MaxSize:    500, // megabytes
+		MaxBackups: 3,
+		MaxAge:     28, //days
+		Compress:   true, // disabled by default
+	})
+
 	num_cpu := flag.Int("num-cpu", 0, "Number of CPUs to use")
 	flag.Parse()
 
 	data := make(chan server.HttpData)
-	go BootServer(data)
+	ch := make(chan preprocess.PacketData)
+	done := make(chan struct{})
 
 	sorter:= getSorter()
-	subspace_keys := utils.GetKeyComb(sorter, 2)
-	min_interval := 0.0
-	max_interval := 1.0
-	interval_length := 0.1
-	dim := 2
-	subspaces := make(map[[2]string]tree.Subspace)
-
-	for _, subspace_key := range subspace_keys{
-		tmp := [2]string{}
-		copy(tmp[:], subspace_key)
-		Int_tree := tree.NewIntervalTree(uint64(dim))
-		grid := tree.NewGrid()
-		ranges := tree.RangeBuilder(min_interval, max_interval, interval_length)
-		intervals := tree.IntervalBuilder(ranges, scale_factor)
-		units := tree.UnitsBuilder(ranges, dim)
-
-		subspace := tree.Subspace{Grid: &grid, Subspace_key: tmp, Scale_factor: scale_factor}
-		subspace.SetIntervalTree(&Int_tree)
-		for _, interval := range intervals{
-			Int_tree.Add(interval)
-		}
-
-		for _, unit := range units{
-			grid.AddUnit(&unit)
-		}
-		grid.SetupGrid(interval_length)
-		subspaces[tmp] = subspace
-	}
-
-	ch := make(chan preprocess.PacketData)
-	acc_c := make(chan preprocess.X_micro_slot)
-	//quit := make(chan int)
-
 	config := process.Config{Min_dense_points: 10, Min_cluster_points: 15, Execution_type: process.PARALLEL, Num_cpu: *num_cpu}
-	go preprocess.WindowTimeSlide(ch, acc_c)
-	go process.UpdateFeatureSpace(acc_c, data, sorter, subspaces, config)
+
+	BootServer(data)
+	subspace_channel_containers := anomalies.ClusteringBuilder(config, done)
+	accumulator_channels := process.UpdateFeatureSpaceBuilder(subspace_channel_containers, sorter, done)
+	preprocess.WindowTimeSlide(ch, accumulator_channels, done)
 
 	handleRead, err := pcap.OpenOffline("C:\\Users\\Jack\\Downloads\\201705021400.pcap")
 
@@ -80,18 +60,77 @@ func main(){
 	for {
 		data, ci, err := handleRead.ReadPacketData()
 		if err != nil && err != io.EOF {
-			close(ch)
-			//quit <- 0
+			close(done)
 			log.Fatal(err)
 		} else if err == io.EOF {
-			close(ch)
-			//quit <- 0
+			close(done)
 			break
 		} else {
 			packet := gopacket.NewPacket(data, layers.LayerTypeEthernet, gopacket.Default)
 			ch <- preprocess.PacketData{Data: packet, Metadata: ci}
 		}
 	}
+
+	////HACK
+	//subspace_keys := [][]string{{"nbSrcs", "avgPktSize"}, {"perICMP", "perSYN"}, {"nbSrcPort", "perICMP"}}
+	//
+	//min_interval := 0.0
+	//max_interval := 1.0
+	//interval_length := 0.1
+	//dim := 2
+	//subspaces := make(map[[2]string]tree.Subspace)
+	//
+	//for _, subspace_key := range subspace_keys{
+	//	tmp := [2]string{}
+	//	copy(tmp[:], subspace_key)
+	//	Int_tree := tree.NewIntervalTree(uint64(dim))
+	//	grid := tree.NewGrid()
+	//	ranges := tree.RangeBuilder(min_interval, max_interval, interval_length)
+	//	intervals := tree.IntervalBuilder(ranges, scale_factor)
+	//	units := tree.UnitsBuilder(ranges, dim)
+	//
+	//	subspace := tree.Subspace{Grid: &grid, Subspace_key: tmp, Scale_factor: scale_factor}
+	//	subspace.SetIntervalTree(&Int_tree)
+	//	for _, interval := range intervals{
+	//		Int_tree.Add(interval)
+	//	}
+	//
+	//	for _, unit := range units{
+	//		grid.AddUnit(&unit)
+	//	}
+	//	grid.SetupGrid(interval_length)
+	//	subspaces[tmp] = subspace
+	//}
+	//
+	//ch := make(chan preprocess.PacketData)
+	//acc_c := make(chan preprocess.X_micro_slot)
+	////quit := make(chan int)
+	//
+	//config := process.Config{Min_dense_points: 10, Min_cluster_points: 15, Execution_type: process.PARALLEL, Num_cpu: *num_cpu}
+	//go preprocess.WindowTimeSlide(ch, acc_c)
+	//go process.UpdateFeatureSpace(acc_c, data, sorter, subspaces, config)
+	//
+	//handleRead, err := pcap.OpenOffline("C:\\Users\\Jack\\Downloads\\201705021400.pcap")
+	//
+	//if(err != nil){
+	//	log.Fatal(err)
+	//}
+	//
+	//for {
+	//	data, ci, err := handleRead.ReadPacketData()
+	//	if err != nil && err != io.EOF {
+	//		close(ch)
+	//		//quit <- 0
+	//		log.Fatal(err)
+	//	} else if err == io.EOF {
+	//		close(ch)
+	//		//quit <- 0
+	//		break
+	//	} else {
+	//		packet := gopacket.NewPacket(data, layers.LayerTypeEthernet, gopacket.Default)
+	//		ch <- preprocess.PacketData{Data: packet, Metadata: ci}
+	//	}
+	//}
 
 	//if handle, err := pcap.OpenLive("eth0", 1600, true, pcap.BlockForever); err != nil {
 	//	panic(err)
