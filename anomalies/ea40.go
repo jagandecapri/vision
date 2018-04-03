b1f0868abbd1da3575b2b894b225bd9cd1dd938e
Jack <jagandecapri@gmail.com>
2018-04-03 22:41:30 +0800

    Commit Concurrent DissimilarityMapContainer

--- anomalies/ea40.go
+++ anomalies/ea40.go
@@ -2,6 +2,7 @@ package anomalies
 
 import (
 	"github.com/jagandecapri/vision/tree"
+	"sync"
 )
 
 type DissimilarityVector struct{
@@ -11,6 +12,43 @@ type DissimilarityVector struct{
 type DissimilarityVectorContainer struct{
 	Id int
 	DissimilarityVectors []DissimilarityVector
+}
+
+type DissimilarityMapContainer struct{
+	sync.RWMutex
+	internal map[int][]DissimilarityVectorContainer
+}
+
+func NewDissimilarityMapContainer() *DissimilarityMapContainer {
+	return &DissimilarityMapContainer{
+		internal: make(map[int][]DissimilarityVectorContainer),
+	}
+}
+
+func (dmc *DissimilarityMapContainer) Load(key int) (value []DissimilarityVectorContainer, ok bool) {
+	dmc.RLock()
+	result, ok := dmc.internal[key]
+	dmc.RUnlock()
+	return result, ok
+}
+
+func (dmc *DissimilarityMapContainer) Len() (value int) {
+	dmc.RLock()
+	result := len(dmc.internal)
+	dmc.RUnlock()
+	return result
+}
+
+func (dmc *DissimilarityMapContainer) Delete(key int) {
+	dmc.Lock()
+	delete(dmc.internal, key)
+	dmc.Unlock()
+}
+
+func (dmc *DissimilarityMapContainer) Store(key int, value DissimilarityVectorContainer) {
+	dmc.Lock()
+	dmc.internal[key] = append(dmc.internal[key], value)
+	dmc.Unlock()
 }
 
 func ComputeDissmilarityVector(subspace tree.Subspace) []DissimilarityVector{
