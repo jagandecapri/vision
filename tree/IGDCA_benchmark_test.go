ad4cb1c477d8a912d5bf16a34510bdb112bb1a04
Jack <jagandecapri@gmail.com>
2018-05-30 23:10:13 +0800

    Add worker pool executor with unique result channels

--- tree/IGDCA_benchmark_test.go
+++ tree/IGDCA_benchmark_test.go
@@ -43,11 +43,12 @@ func setupGrids(min_points int, max_points int, num_grids int) (grids []*Grid, m
 func BenchmarkIGDCAVaryPoints(b *testing.B){
 	tests := []struct{
 		name string
-		fun func(*testing.B, []*Grid, int, int)
+		benchmark BenchmarkInterface
 	}{
-		{"Sequential", Sequential},
-		{"Concurrent", Concurrent},
-		{"Worker", Worker},
+		{"Sequential", &SequentialExecutor{}},
+		{"Concurrent", &ConcurrentExecutor{}},
+		{"WorkerPoolExecutor", &WorkerPoolExecutor{}},
+		{"WorkerPoolUniqueResChannelExecutor", & WorkerPoolUniqueResChannelExecutor{}},
 	}
 
 	for _, test := range tests{
@@ -58,10 +59,14 @@ func BenchmarkIGDCAVaryPoints(b *testing.B){
 			b.Run(fmt.Sprintf("%v/min-max=%d-%d/num_grids=%d", test.name, min, max, num_grids), func(b *testing.B){
 				b.StopTimer()
 				grids, min_dense_points, min_cluster_points := setupGrids(min, max, 8)
+				test.benchmark.Init(grids, min_dense_points, min_cluster_points)
 				b.StartTimer()
 				for n := 0; n < b.N; n++{
-					test.fun(b, grids, min_dense_points, min_cluster_points)
+					test.benchmark.Run(b, grids, min_dense_points, min_cluster_points)
 				}
+				b.StopTimer()
+				test.benchmark.Clean()
+				b.StartTimer()
 			})
 		}
 	}
@@ -70,11 +75,12 @@ func BenchmarkIGDCAVaryPoints(b *testing.B){
 func BenchmarkIGDCAVarySubspaces(b *testing.B){
 	tests := []struct{
 		name string
-		fun func(*testing.B, []*Grid, int, int)
+		benchmark BenchmarkInterface
 	}{
-		{"Sequential", Sequential},
-		{"Concurrent", Concurrent},
-		{"Worker", Worker},
+		{"Sequential", &SequentialExecutor{}},
+		{"Concurrent", &ConcurrentExecutor{}},
+		{"WorkerPoolExecutor", &WorkerPoolExecutor{}},
+		{"WorkerPoolUniqueResChannelExecutor", & WorkerPoolUniqueResChannelExecutor{}},
 	}
 
 	for _, test := range tests{
@@ -85,23 +91,43 @@ func BenchmarkIGDCAVarySubspaces(b *testing.B){
 			b.Run(fmt.Sprintf("%v/min-max=%d-%d/num_grids=%d", test.name, min, max, num_grids), func(b *testing.B){
 				b.StopTimer()
 				grids, min_dense_points, min_cluster_points := setupGrids(10, 100,  num_grids)
+				test.benchmark.Init(grids, min_dense_points, min_cluster_points)
 				b.StartTimer()
 				for n := 0; n < b.N; n++{
-					test.fun(b, grids, min_dense_points, min_cluster_points)
+					test.benchmark.Run(b, grids, min_dense_points, min_cluster_points)
 				}
+				b.StopTimer()
+				test.benchmark.Clean()
+				b.StartTimer()
 			})
 		}
 	}
 }
 
-func Sequential(b *testing.B, grids []*Grid, min_dense_points int, min_cluster_points int){
+type BenchmarkInterface interface{
+	Init([]*Grid, int, int)
+	Run(*testing.B, []*Grid, int, int)
+	Clean()
+}
+
+type SequentialExecutor struct{}
+
+func (s *SequentialExecutor) Init(grids []*Grid, min_dense_points int, min_cluster_points int){}
+
+func (s *SequentialExecutor) Run(b *testing.B, grids []*Grid, min_dense_points int, min_cluster_points int){
 	for m := 0; m < len(grids); m++{
 		grid := grids[m]
 		IGDCA(grid, min_dense_points, min_cluster_points)
 	}
 }
 
-func Concurrent(b *testing.B, grids []*Grid, min_dense_points int, min_cluster_points int){
+func (s *SequentialExecutor) Clean(){}
+
+type ConcurrentExecutor struct{}
+
+func (c *ConcurrentExecutor) Init(grids []*Grid, min_dense_points int, min_cluster_points int){}
+
+func (c *ConcurrentExecutor) Run(b *testing.B, grids []*Grid, min_dense_points int, min_cluster_points int){
 	wg := sync.WaitGroup{}
 	wg.Add(len(grids))
 
@@ -117,30 +143,133 @@ func Concurrent(b *testing.B, grids []*Grid, min_dense_points int, min_cluster_p
 	wg.Wait()
 }
 
-func Worker(b *testing.B, grids []*Grid, min_dense_points int, min_cluster_points int){
-	b.StopTimer()
-	jobs := make(chan *Grid, len(grids))
-	results := make(chan struct{}, len(grids))
+func (c *ConcurrentExecutor) Clean(){}
 
-	for w := 1; w <= 10; w++ {
-		go worker(w, min_dense_points, min_cluster_points, jobs, results)
-	}
-	b.StartTimer()
+type WorkerPoolExecutor struct{
+	jobs chan *Grid
+	results chan struct{}
+}
 
-	for m := 0; m < len(grids); m++{
-		grid := grids[m]
-		jobs <- grid
-	}
-	close(jobs)
-
-	for m := 0; m < len(grids); m++{
-		<-results
+func (w *WorkerPoolExecutor) Init(grids []*Grid, min_dense_points int, min_cluster_points int){
+	w.jobs = make(chan *Grid, len(grids))
+	w.results = make(chan struct{}, len(grids))
+	num_workers := 4
+	for i := 0; i < num_workers; i++ {
+		go Worker(i, min_dense_points, min_cluster_points, w.jobs, w.results)
 	}
 }
 
-func worker(id int, min_dense_points int, min_cluster_points int, jobs <-chan *Grid, results chan<- struct{}) {
-	for j := range jobs {
-		IGDCA(j, min_dense_points, min_cluster_points)
-		results <- struct{}{}
+func (w *WorkerPoolExecutor) Run(b *testing.B, grids []*Grid, min_dense_points int, min_cluster_points int){
+	for m := 0; m < len(grids); m++{
+		grid := grids[m]
+		w.jobs <- grid
 	}
+
+	for m := 0; m < len(grids); m++{
+		<-w.results
+	}
+}
+
+func Worker(id int, min_dense_points int, min_cluster_points int, jobs <-chan *Grid, results chan<- struct{}) {
+	for{
+		select{
+			case j, ok := <-jobs:
+				if ok{
+					IGDCA(j, min_dense_points, min_cluster_points)
+					results <- struct{}{}
+				} else {
+					return
+				}
+			default:
+		}
+	}
+}
+
+
+func (w *WorkerPoolExecutor) Clean(){
+	close(w.jobs)
+	close(w.results)
+}
+
+type WorkerPoolUniqueResChannelExecutor struct{
+	jobs chan *Grid
+	done chan struct{}
+	final_out <-chan struct{}
+}
+
+func (w *WorkerPoolUniqueResChannelExecutor) Init(grids []*Grid, min_dense_points int, min_cluster_points int){
+	w.jobs = make(chan *Grid, len(grids))
+	w.done = make(chan struct{})
+	num_workers := 4
+
+	var cs []chan struct{}
+	for i := 0; i < num_workers; i++ {
+		c := WorkerUnique(i, min_dense_points, min_cluster_points, w.done, w.jobs)
+		cs = append(cs, c)
+	}
+
+	w.final_out = Receiver(w.done, cs...)
+}
+
+func (w *WorkerPoolUniqueResChannelExecutor) Run(b *testing.B, grids []*Grid, min_dense_points int, min_cluster_points int){
+	for m := 0; m < len(grids); m++{
+		grid := grids[m]
+		w.jobs <- grid
+	}
+
+	for m := 0; m < len(grids); m++{
+		<-w.final_out
+	}
+}
+
+func (w *WorkerPoolUniqueResChannelExecutor) Clean(){
+	close(w.done)
+}
+
+
+func WorkerUnique(id int, min_dense_points int, min_cluster_points int, done <-chan struct{}, jobs <-chan *Grid) chan struct{}{
+	out := make(chan struct{})
+	go func(){
+		defer close(out)
+		for{
+			select{
+			case j := <-jobs:
+					IGDCA(j, min_dense_points, min_cluster_points)
+					out <- struct{}{}
+			case <-done:
+				return
+			}
+		}
+	}()
+	return out
+}
+
+func Receiver(done <-chan struct{}, cs ...chan struct{}) <-chan struct{}{
+	wg := sync.WaitGroup{}
+	out := make(chan struct{})
+
+	output := func(c <-chan struct{}){
+		defer wg.Done()
+		for n := range c {
+			select {
+			case out <- n:
+			case <-done:
+				return
+			}
+		}
+	}
+
+	wg.Add(len(cs))
+
+	for _, c := range cs {
+		go output(c)
+	}
+
+	// Start a goroutine to close out once all the output goroutines are
+	// done.  This must start after the wg.Add call.
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
 }
